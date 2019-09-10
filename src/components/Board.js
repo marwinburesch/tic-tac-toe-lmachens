165ec379979f160185b1a9361d5516e665b40307
lmachens <leon.machens@googlemail.com>
2019-09-10 16:51:08 +0200

    avoid handleClick if winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Square from "./Square";
+import calculateWinner from "../api/calculateWinner";
 
 function Board() {
   const [state, setState] = React.useState({
@@ -8,7 +9,7 @@ function Board() {
   });
 
   function handleClick(index) {
-    if (state.squares[index]) {
+    if (state.squares[index] || calculateWinner(state.squares)) {
       return;
     }
     const squaresCopy = state.squares.slice();
@@ -17,6 +18,15 @@ function Board() {
       squares: squaresCopy,
       xIsNext: !state.xIsNext
     });
+
+    // const winner = calculateWinner(squaresCopy);
+    // if (winner) {
+    //   alert("Congratulations " + winner);
+    //   setState({
+    //     squares: Array(9).fill(null),
+    //     xIsNext: true
+    //   });
+    // }
   }
 
   function renderSquare(index) {
@@ -25,7 +35,13 @@ function Board() {
     );
   }
 
-  const status = `Next player: ${state.xIsNext ? "X" : "O"}`;
+  const winner = calculateWinner(state.squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${state.xIsNext ? "X" : "O"}`;
+  }
 
   return (
     <div>
