a01fcb69ea56ac73283bc587d56427659b79905c
lmachens <leon.machens@googlemail.com>
2019-09-10 11:48:47 +0200

    add value to Square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,33 @@
 import React from "react";
 import Square from "./Square";
 
-export default function Board() {
+function renderSquare(index) {
+  return <Square value={index} />;
+}
+
+function Board() {
   const status = "Next player: X";
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
 }
+
+export default Board;
